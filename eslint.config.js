486a1ed9999bf478805eaba5bdf323c71091f2d0
JMC816 <jangmincheol816@gmail.com>
2025-03-09 14:43:14 +0900

    초기 세팅

---
+++ eslint.config.js
@@ -0,0 +1,55 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
+import pluginTailwind from 'eslint-plugin-tailwindcss';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.app.json',
+        },
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+  {
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+    },
+  },
+  {
+    plugins: {
+      tailwindcss: pluginTailwind,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  },
+];
---
+++ src/app/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="h-4">시작</div>;
+}
+
+export default App;

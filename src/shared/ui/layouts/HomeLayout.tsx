ad26ce2083748c5b6c358a1641f7303636e9058b
JMC816 <jangmincheol816@gmail.com>
2025-03-13 10:19:56 +0900

    fix: header 제거

--- src/shared/ui/layouts/HomeLayout.tsx
+++ src/shared/ui/layouts/HomeLayout.tsx
@@ -1,11 +1,9 @@
 import { Outlet } from 'react-router-dom';
-import Header from '../Header';
 import NavBar from '../NavBar';
 
 const HomeLayout = () => {
   return (
     <>
-      <Header />
       <Outlet />
       <NavBar />
     </>

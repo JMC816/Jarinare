8531bbeb5a5095124bdf920aa641f62c0aa9e762
JMC816 <jangmincheol816@gmail.com>
2025-04-02 09:41:01 +0900

    fix: 파일명 변경

--- src/widgets/TicketReserve/constants/ReserveConstants.ts
+++ src/widgets/TicketReserve/constants/ReserveConstants.ts
@@ -1,3 +1,39 @@
+import square from '@/assets/icons/square.png';
+import group from '@/assets/icons/group.png';
+import calendar from '@/assets/icons/calendar.png';
+import person from '@/assets/icons/person.png';
+
+export const ReserveWayArray = [
+  {
+    icon: square,
+    text: '출발지를 선택하세요',
+    attribute: '출발',
+    buttonText: '변경',
+    modal: 'StartPlaceModal',
+  },
+  {
+    icon: group,
+    text: '도착지를 선택하세요',
+    attribute: '도착',
+    buttonText: '변경',
+    modal: 'EndPlaceModal',
+  },
+  {
+    icon: calendar,
+    text: '날짜를 선택하세요',
+    attribute: '가는날',
+    buttonText: '변경',
+    modal: 'DayModal',
+  },
+  {
+    icon: person,
+    text: '인원을 선택하세요',
+    attribute: '인원',
+    buttonText: '변경',
+    modal: 'CountModal',
+  },
+] as const;
+
 export const TrainListArray = [
   {
     startTime: '오전 5:03',

22806e3a42d991c42473cf4276398d1cc731bd6f
JMC816 <jangmincheol816@gmail.com>
2025-04-20 03:57:45 +0900

    fix: 로그인 시 모달 변경 이슈로 input 엔터키 방지

--- src/shared/ui/AuthInput.tsx
+++ src/shared/ui/AuthInput.tsx
@@ -5,6 +5,11 @@ const AuthInput = ({ placeholder, name }: AuthInputProps) => {
     <>
       <input
         {...name}
+        onKeyDown={(e) => {
+          if (e.key == 'Enter') {
+            e.preventDefault();
+          }
+        }}
         placeholder={placeholder}
         className="h-12 w-[300px] items-center justify-center rounded-sm bg-white px-[15px] text-tiny font-bold text-lightGray"
       />

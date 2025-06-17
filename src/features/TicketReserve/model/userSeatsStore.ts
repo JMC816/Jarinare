a21f8a421bdbf4609a402355a8fb7790bdab8814
JMC816 <jangmincheol816@gmail.com>
2025-06-18 02:58:13 +0900

    fix: 각 시간대별 좌석 데이터 가져올 때 docId가 동일한 오류 수정 및 좌석 정보 추가

--- src/features/TicketReserve/model/userSeatsStore.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand';
-import { SeatType } from '@/entities/Seat/types/seatType';
-import { UserSeatsStoreType } from '../types/userSeatsStoreType';
-
-export const userSeatsStore = create<UserSeatsStoreType>((set) => ({
-  userSeats: [],
-  setUserSeats: (userSeats: SeatType[]) => set(() => ({ userSeats })),
-}));

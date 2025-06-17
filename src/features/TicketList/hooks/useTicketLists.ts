94b23e29de4f87b9564531f72f1d732194ebf480
JMC816 <jangmincheol816@gmail.com>
2025-06-18 03:02:51 +0900

    fix: 좌석 데이터 수정

--- src/features/TicketList/hooks/useTicketLists.ts
+++ src/features/TicketList/hooks/useTicketLists.ts
@@ -1,11 +1,19 @@
 import { SeatType } from '@/entities/Seat/types/seatType';
 import { useSeatQueryData } from '@/features/TicketReserve/hooks/useSeatQueryData';
+import { auth } from '@/shared/firebase/firebase';
 
 export const useTicketLists = () => {
-  const { userSeats } = useSeatQueryData();
+  const { seatsAllInfo } = useSeatQueryData();
+  const user = auth.currentUser;
+
+  if (!user) {
+    return;
+  }
+
+  const filtered = seatsAllInfo.filter((item) => item.userId === user.uid);
 
   // 생성 시간을 key로 하여 2개 이상의 좌석을 선택했을 경우 하나의 배열로 반환
-  const groupSeats = userSeats.reduce<Record<number, SeatType[]>>(
+  const groupSeats = filtered.reduce<Record<number, SeatType[]>>(
     (acc, current) => {
       acc[current.createAt] = acc[current.createAt] || [];
       acc[current.createAt].push(current);

249fda679a316520e3101f302b1a30532f3bd425
JMC816 <jangmincheol816@gmail.com>
2025-09-12 20:12:17 +0900

    refator: 좌석 변경 요청 시 모달 조건 추가

--- src/widgets/TicketChange/ui/RequestChangeModal.tsx
+++ src/widgets/TicketChange/ui/RequestChangeModal.tsx
@@ -1,11 +1,25 @@
 import request from '@/assets/icons/request.png';
+import { seatsStateStore } from '@/features/TicketChange/models/seatsStateStore';
+import { seatsTargetStore } from '@/features/TicketChange/models/seatsTargetStore';
 
 const RequestChangeModal = () => {
+  const { seatsState } = seatsStateStore();
+
+  const target = seatsTargetStore().seatsTarget;
+
+  const emptySeatsTarget = Object.entries(seatsState)
+    .filter(([, value]) => value === true)
+    .map(([key]) => key);
+
   return (
     <div className="flex h-full w-full flex-col items-center justify-center bg-darkGray/50">
       <div className="flex h-[150px] w-[200px] flex-col items-center justify-center rounded-2xl bg-white font-bold text-blue">
         <img src={request} width={40} height={40} />
-        <span className="mt-[25px]">자리 요청을 보냈습니다!</span>
+        <span className="mt-[25px]">
+          {target.length === emptySeatsTarget.length
+            ? '자리가 변경되었습니다!'
+            : '자리 요청을 보냈습니다!'}
+        </span>
       </div>
     </div>
   );

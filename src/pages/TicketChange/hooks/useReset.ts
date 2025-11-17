e7b9c4bcf2ef95c9de8eb17dc0f5d11fa7e30a58
JMC816 <jangmincheol816@gmail.com>
2025-11-17 13:39:19 +0900

    fix: 의존성 배열 추가

--- src/pages/TicketChange/hooks/useReset.ts
+++ src/pages/TicketChange/hooks/useReset.ts
@@ -1,11 +1,13 @@
 import { useMixSeats } from '@/features/TicketChange/hooks/useMixSeats';
 import { seatsChangeTargetStore } from '@/features/TicketChange/models/seatsChangeTargetStore';
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 // 이전 페이지로 이동하거나 해당 주소로 이동 시 이전에 좌석 선택창에서 선택했던 좌석들 초기화
 export const useReset = () => {
   const { setKeepSeats, setShareKeepSeats } = useMixSeats();
   const { setSeatsChangeTarget } = seatsChangeTargetStore();
+  const locaiton = useLocation();
 
   useEffect(() => {
     if (location.pathname === '/ticketList') {
@@ -13,5 +15,10 @@ export const useReset = () => {
       setKeepSeats([]);
       setShareKeepSeats([]);
     }
-  }, []);
+  }, [
+    locaiton.pathname,
+    setSeatsChangeTarget,
+    setKeepSeats,
+    setShareKeepSeats,
+  ]);
 };

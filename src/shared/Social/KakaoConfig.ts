99afab43b8f4527f2ccf2552dfb7e875ed7ac42d
JMC816 <jangmincheol816@gmail.com>
2026-04-17 00:07:46 +0900

    fix: 카카오 로그인 오류 수정

--- src/shared/Social/KakaoConfig.ts
+++ src/shared/Social/KakaoConfig.ts
@@ -1,4 +1,5 @@
 export const REST_API_KEY = import.meta.env.VITE_APP_REST_API_KEY;
-export const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
-export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 export const CLIENT_SECRET = import.meta.env.VITE_APP_CLIENT_SECRET;
+
+export const REDIRECT_URI = `${window.location.origin}/oauth/kakao/callback`;
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=code`;

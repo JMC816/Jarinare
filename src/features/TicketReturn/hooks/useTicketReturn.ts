7747feacf4a5b4d1be592d139959bdb7b7a7530a
JMC816 <jangmincheol816@gmail.com>
2026-06-23 17:49:40 +0900

    fix: 승차권 반환 후 페이지 이탈 오류 수정

--- src/features/TicketReturn/hooks/useTicketReturn.ts
+++ src/features/TicketReturn/hooks/useTicketReturn.ts
@@ -5,11 +5,9 @@ import {
 } from '@/features/TicketReserve/hooks/useAllSeatsInfo';
 import { db } from '@/shared/firebase/firebase';
 import { deleteDoc, doc } from 'firebase/firestore';
-import { useNavigate } from 'react-router-dom';
 
 export const useTicketReturn = () => {
   const { seatsReturnData } = seatsReturnDataStore();
-  const navigate = useNavigate();
 
   // 선택한 승차권 제거
   const handleDeleteSeats = async () => {
@@ -31,7 +29,6 @@ export const useTicketReturn = () => {
       );
       clearAllSeatsCache();
       prefetchAllSeats();
-      navigate(-1);
     } catch (e) {
       console.log(e);
     }

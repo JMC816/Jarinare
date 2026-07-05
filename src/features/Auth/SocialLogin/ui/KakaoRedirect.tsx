f1d9b5bda512d548ab33ef968b08075c4cdc4bbc
JMC816 <jangmincheol816@gmail.com>
2026-07-05 15:57:48 +0900

    feat: 카카오 로그인 시 로딩 스크린 수평 수직 중앙 정렬

--- src/features/Auth/SocialLogin/ui/KakaoRedirect.tsx
+++ src/features/Auth/SocialLogin/ui/KakaoRedirect.tsx
@@ -5,7 +5,7 @@ import { useKakaoRedirect } from '../hooks/useKakaoLogin';
 const KakaoRedirect = () => {
   useKakaoRedirect();
   return (
-    <div className="flex h-full flex-col items-center justify-center">
+    <div className="flex h-screen w-screen items-center justify-center">
       <img src={loading} className="h-12 w-12 animate-spin" />
     </div>
   );
